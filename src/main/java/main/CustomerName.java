b07cccc16e56656ee151682b6fc3fac4426c5629
MatoSuraj <48233839+MatoSuraj@users.noreply.github.com>
2019-06-14 21:00:09 +0530

    completed Successfully

--- src/main/java/main/CustomerName.java
+++ src/main/java/main/CustomerName.java
@@ -2,16 +2,19 @@ package main;
 
 import java.util.Scanner;
 
-public class CustomerName{
+public class CustomerName
+{
 	String name;
 
-	public String getName() {
+	public String getName() 
+	{
 		
 		return name;
 	}
 
-	public void setName(String name) {
-		//write your code here
+	public void setName(String name)
+	{
+		this.name = name;
 	}
 	
 }
